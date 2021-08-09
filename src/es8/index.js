727ebb69be49822734fbba68bdd93ee74f8d2fe6
aaxlss <axl9305@gmail.com>
2021-08-09 23:13:46 +0100

    Using object entries to generate an array of arrays with key value and values to get the values from the object

---
+++ src/es8/index.js
@@ -0,0 +1,20 @@
+const data = {
+    frontend: 'axel',
+    backend: 'alex',
+    design: 'axl',
+}
+
+const entries = Object.entries(data);
+
+console.log(entries);
+console.log(entries.length);
+
+
+const data = {
+    frontend: 'axel',
+    backend: 'alex',
+    design: 'axl',
+}
+
+const values = Object.values(data);
+console.log(values);
