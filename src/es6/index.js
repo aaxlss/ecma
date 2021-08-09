20e02807d1c58141c2f4e7a5a6883a915410f0c5
aaxlss <axl9305@gmail.com>
2021-08-09 23:01:56 +0100

    creating generator functions

--- src/es6/index.js
+++ src/es6/index.js
@@ -145,3 +145,25 @@ class Calculater {
 
 const calc = new Calculater();
 console.log(calc.sum(2,2));
+
+// modulos
+
+import hello from "./module";
+
+hello();
+
+
+//Generator
+function* helloWorld(){
+    if(true){
+        yield 'Hello,';
+    }
+    if (true) {
+        yield 'World';
+    }
+};
+
+const generatorHello = helloWorld();
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
+console.log(generatorHello.next().value);
