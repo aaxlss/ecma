9d2c98f42e6f6831da3c48efa92219799d84c06d
aaxlss <axl9305@gmail.com>
2021-08-09 23:26:39 +0100

    Working with async await and try catch

--- src/es8/index.js
+++ src/es8/index.js
@@ -22,3 +22,30 @@ console.log(values);
 const string = 'hello';
 console.log(string.padStart(7,'hi '));
 console.log(string.padEnd(12, '----'));
+
+////////////////
+const helloWorld = () => {
+    return new Promise((result, reject) => {
+        (false) 
+        ? setTimeout(() => result('Hello World'), 3000)
+        : reject(new Error('Error'));
+    });
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.log(error);
+    }
+}
+anotherFunction();
