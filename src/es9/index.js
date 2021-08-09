772749a3788a2768709dbe2de4618bb3fc406bde
aaxlss <axl9305@gmail.com>
2021-08-09 23:44:18 +0100

    Using regex

--- src/es9/index.js
+++ src/es9/index.js
@@ -20,7 +20,7 @@ console.log(obj1);
 
 const helloWorld = () => {
     return new Promise((resolve, reject) => {
-        (true)
+        (false)
         ?resolve('hello World')
         :resolve(new Error('test error'))
     });
@@ -30,3 +30,15 @@ helloWorld()
 .then(response => console.log(response))
 .catch(error => console.log(error))
 .finally(() => console.log('finish'));
+
+
+///
+const regexData = /([0-9]{4}-([0-9]){2}-([0-9]{2}))/
+
+const match = regexData.exec('2018-04-20');
+
+const year = match[1];
+const month = match[2];
+const day = match[3];
+
+console.log(year, month, day);
